0176c569a0d9d524ca0e5ffab3b9209d01f0b987
DJPhonk <hakersenos57@gmail.com>
2025-04-27 18:32:56 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,1 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/TsuoLoader/refs/heads/main/Tsuo.lua"))()
